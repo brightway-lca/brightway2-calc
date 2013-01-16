5be53e92ce8b680e999a88304450829965455e34
Chris Mutel <cmutel@gmail.com>
2013-01-16 09:22:07 -0500

    Start calc documentation

--- docs/conf.py
+++ docs/conf.py
@@ -11,12 +11,11 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+from os.path import abspath, dirname
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# Make sure we use this copy of Brightway2
+sys.path.insert(1, abspath(dirname(dirname(__file__))))
 
 # -- General configuration -----------------------------------------------------
 
---
+++ docs/technical.rst
@@ -0,0 +1,25 @@
+Technical guide
+***************
+
+Static calculations
+===================
+
+
+Uncertainty analysis
+====================
+
+
+Calculation classes
+===================
+
+.. autoclass:: bw2calc.LCA
+    :members:
+
+.. autoclass:: bw2calc.SimpleRegionalizedLCA
+    :members:
+
+.. autoclass:: bw2calc.MonteCarloLCA
+    :members:
+
+.. autoclass:: bw2calc.ParallelMonteCarlo
+    :members:
