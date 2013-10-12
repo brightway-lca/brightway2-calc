93ab4e62eba68d5e6c670e28fc777d210c877f9f
Chris Mutel <cmutel@gmail.com>
2013-10-12 21:37:14 +0200

    Cleanup of GraphTraversal

--- bw2calc/__init__.py
+++ bw2calc/__init__.py
@@ -3,8 +3,7 @@ from .lca import LCA
 from .simple_regionalized import SimpleRegionalizedLCA
 from .monte_carlo import MonteCarloLCA, ParallelMonteCarlo, MultiMonteCarlo
 from .mc_vector import ParameterVectorLCA
-from .graph_traversal import GraphTraversal, edge_cutter, node_pruner, \
-    d3_fd_graph_formatter
+from .graph_traversal import GraphTraversal
 from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
 
 __version__ = (0, 10, 2)
--- bw2calc/graph_traversal.py
+++ bw2calc/graph_traversal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*
 from __future__ import division
 from . import LCA
-from brightway2 import mapping, Database
+from brightway2 import Database
 from heapq import heappush, heappop
 import numpy as np
 
@@ -33,8 +33,6 @@ Returns:
     Dictionary of nodes, edges, LCA object, and number of LCA calculations.
 
         """
-        counter = 0
-
         lca, supply, score = self.build_lca(demand, method)
         if score == 0:
             raise ValueError("Zero total LCA score makes traversal impossible")
@@ -48,9 +46,8 @@ Returns:
         heap, nodes, edges = self.initialize_heap(
             demand, lca, supply, characterized_biosphere)
         nodes, edges, counter = self.traverse(
-            heap, nodes, edges, counter, max_calc, cutoff, score, supply,
+            heap, nodes, edges, 0, max_calc, cutoff, score, supply,
             characterized_biosphere, lca)
-        nodes = self.add_metadata(nodes, lca)
 
         return {
             'nodes': nodes,
@@ -68,12 +65,17 @@ Populates the heap with each activity in ``demand``. Initial nodes are the *func
 The *functional unit* is an abstract dataset (as it doesn't exist in the matrix), and is assigned the index ``-1``.
 
         """
-        heap, nodes, edges = [], {}, []
+        heap, edges = [], []
+        nodes = {-1: {
+            'amount': 1,
+            'cum': lca.score,
+            'ind': 1e-6 * lca.score
+        }}
         for activity, value in demand.iteritems():
-            index = lca.technosphere_dict[mapping[activity]]
+            index = lca.technosphere_dict[activity]
             heappush(heap, (1, index))
             nodes[index] = {
-                "amount": supply[index],
+                "amount": float(supply[index]),
                 "cum": self.cumulative_score(
                     index, supply, characterized_biosphere, lca),
                 "ind": self.unit_score(index, supply, characterized_biosphere)
@@ -82,6 +84,7 @@ The *functional unit* is an abstract dataset (as it doesn't exist in the matrix)
                 "to": -1,
                 "from": index,
                 "amount": value,
+                "exc_amount": value,
                 "impact": lca.score,
             })
         return heap, nodes, edges
@@ -91,6 +94,7 @@ The *functional unit* is an abstract dataset (as it doesn't exist in the matrix)
         lca = LCA(demand, method)
         lca.lci()
         lca.lcia()
+        lca.fix_dictionaries()
         lca.decompose_technosphere()
         return lca, lca.solve_linear_system(), lca.score
 
@@ -109,6 +113,8 @@ The *functional unit* is an abstract dataset (as it doesn't exist in the matrix)
         """
 Build a directed graph by traversing the supply chain.
 
+Node ids are actually technosphere row/col indices, which makes lookup easier.
+
 Returns:
     (nodes, edges, number of calculations)
 
@@ -119,8 +125,8 @@ Returns:
             col = lca.technosphere_matrix[:, parent_index].tocoo()
             # Multiply by -1 because technosphere values are negative
             # (consumption of inputs)
-            children = [(col.row[i], -1 * col.data[i]) for i in xrange(
-                col.row.shape[0])]
+            children = [(int(col.row[i]), float(-1 * col.data[i]))
+                for i in xrange(col.row.shape[0])]
             for activity, amount in children:
                 # Skip values on technosphere diagonal or coproducts
                 if activity == parent_index or amount <= 0:
@@ -134,13 +140,14 @@ Returns:
                 edges.append({
                     "to": parent_index,
                     "from": activity,
-                    # The cumulative impact directly due to this link (weight)
                     # Amount of this link * amount of parent demanding link
                     "amount": amount * nodes[parent_index]["amount"],
-                    # Amount of this input
-                    "impact": amount * nodes[parent_index]["amount"]
+                    # Raw exchange value
+                    "exc_amount": amount,
+                    # Amount of dataset demanded
+                    "impact": float(amount * nodes[parent_index]["amount"]
                     # times impact per unit of this input
-                    * cumulative_score / supply[activity]
+                    * cumulative_score / supply[activity])
                 })
                 # Want multiple incoming edges, but don't add existing node
                 if activity in nodes:
@@ -158,94 +165,3 @@ Returns:
                 heappush(heap, (1 / cumulative_score, activity))
 
         return nodes, edges, counter
-
-    def add_metadata(self, nodes, lca):
-        """Add metadata to nodes, like name and category."""
-        rm = dict([(v, k) for k, v in mapping.data.iteritems()])
-        rt = dict([(v, k) for k, v in lca.technosphere_dict.iteritems()])
-        lookup = dict([(index, self.get_code(index, rm, rt)) for index in nodes if index != -1])
-        new_nodes = [(-1, {
-            "code": "fu",
-            "cum": lca.score,
-            "ind": 1e-6 * lca.score,
-            "amount": 1,
-            "name": "Functional unit",
-            "cat": "Functional unit"
-        })]
-        for key, value in nodes.iteritems():
-            if key == -1:
-                continue
-            code = lookup[key]
-            db_data = Database(code[0]).load()
-            value.update({
-                "code": code,
-                "name": db_data[code]["name"],
-                "cat": db_data[code]["categories"][0],
-            })
-            new_nodes.append((key, value))
-        return dict(new_nodes)
-
-    def get_code(self, index, rev_mapping, rev_tech):
-        """Turn technosphere index into database identifier."""
-        return rev_mapping[rev_tech[index]]
-
-
-def edge_cutter(nodes, edges, total, limit=0.0025):
-    """The default graph traversal includes links which might be of small magnitude. This function cuts links that have small cumulative impact."""
-    return [e for e in edges if e["impact"] >= (total * limit)]
-
-
-def node_pruner(nodes, edges):
-    """Remove nodes which have no links remaining after edge cutting."""
-    good_nodes = set([e["from"] for e in edges]).union(
-        set([e["to"] for e in edges]))
-    return dict([(k, v) for k, v in nodes.iteritems() if k in good_nodes])
-
-
-def extract_edges(arr, mapping, ignore):
-    edges = []
-    for i in range(arr.shape[0]):
-        if mapping[i] in ignore:
-            continue
-        for j in range(arr.shape[1]):
-            if mapping[j] in ignore or i == j or arr[i, j] == 0:
-                continue
-            edges.append((mapping[j], mapping[i], float(arr[i, j])))
-    return edges
-
-
-def rationalize_supply_chain(nodes, edges, total, limit=0.005):
-    """
-This class takes nodes and edges, and removes nodes to edges with low individual scores and reroutes the edges.
-    """
-    nodes_to_delete = [key for key, value in nodes.iteritems() if
-                       value["ind"] < (total * limit) and key != -1]
-    size = len(nodes)
-    arr = np.zeros((size, size), dtype=np.float32)
-    arr_map = dict([(key, index) for index, key in enumerate(sorted(nodes.keys()))])
-    rev_map = dict([(v, k) for k, v in arr_map.iteritems()])
-    for outp, inp, amount in edges:
-        arr[arr_map[inp], arr_map[outp]] = amount
-    for node in nodes_to_delete:
-        index = arr_map[node]
-        increment = (arr[:, index].reshape((-1, 1)) * arr[index, :].reshape((1, -1)))
-        arr += increment
-    new_edges = []
-    new_edges = extract_edges(arr, rev_map, nodes_to_delete)
-    new_nodes = dict([(k, v) for k, v in nodes.iteritems() if k not in nodes_to_delete])
-    return new_nodes, new_edges
-
-
-def d3_fd_graph_formatter(nodes, edges, total):
-        # Sort node ids by highest cumulative score first
-        node_ids = [x[1] for x in sorted(
-            [(v["cum"], k) for k, v in nodes.iteritems()])]
-        new_nodes = [nodes[i] for i in node_ids]
-        lookup = dict([(key, index) for index, key in enumerate(node_ids)])
-        new_edges = [{
-            "source": lookup[e["to"]],
-            "target": lookup[e["from"]],
-            "amount": e["impact"]
-        } for e in edges]
-        return {"edges": new_edges, "nodes": new_nodes,
-                "total": total}
