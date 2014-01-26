106873943bc05d4973156eca8a8f9e6207660d56
Chris Mutel <cmutel@gmail.com>
2014-01-26 07:09:20 +0100

    Removed simple regionalized LCA, prepare for bw2regional

--- bw2calc/__init__.py
+++ bw2calc/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*
 from .lca import LCA
-from .simple_regionalized import SimpleRegionalizedLCA
 from .monte_carlo import MonteCarloLCA, ParallelMonteCarlo, MultiMonteCarlo
 from .mc_vector import ParameterVectorLCA
 from .graph_traversal import GraphTraversal
