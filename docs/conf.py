a27af2b541bfdf2de8adc6357d9a577b9f970845
Chris Mutel <cmutel@gmail.com>
2014-02-27 07:30:34 +0100

    0.12

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@
 # built documents.
 #
 # The short X.Y version.
-version = '0.11'
+version = '0.12'
 # The full version, including alpha/beta/rc tags.
-release = '0.11.1'
+release = '0.12'
 
 import sys
 from os.path import abspath, dirname
