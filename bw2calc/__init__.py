458a73fd05f3c21bd17ddf46efe3fa8ad4ef3f65
Chris Mutel <cmutel@gmail.com>
2014-04-26 10:04:22 +0200

    0.14

--- bw2calc/__init__.py
+++ bw2calc/__init__.py
@@ -5,4 +5,4 @@ from .mc_vector import ParameterVectorLCA
 from .graph_traversal import GraphTraversal
 from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
 
-__version__ = (0, 13)
+__version__ = (0, 14)
