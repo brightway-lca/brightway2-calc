84baf9560db779e9b795800b6775626b57d9197a
Chris Mutel <cmutel@gmail.com>
2014-06-11 11:28:20 +0200

    Convert tests to use .filepath_processed

--- bw2calc/tests/matrices.py
+++ bw2calc/tests/matrices.py
@@ -26,15 +26,18 @@ class MatrixBuilderTestCase(BW2DataTest):
             pickle.dump(b, f, protocol=pickle.HIGHEST_PROTOCOL)
         self.assertTrue(np.allclose(
             a,
-            MatrixBuilder.load(config.dir, ["a"])
+            MatrixBuilder.load(config.dir, [a_db.filepath_processed()])
         ))
         self.assertTrue(np.allclose(
             b,
-            MatrixBuilder.load(config.dir, ["b"])
+            MatrixBuilder.load(config.dir, [b_db.filepath_processed()])
         ))
         self.assertTrue(np.allclose(
             c,
-            MatrixBuilder.load(config.dir, ["a", "b"])
+            MatrixBuilder.load(
+                config.dir,
+                [a_db.filepath_processed(), b_db.filepath_processed()]
+            )
         ))
 
     def test_build_one_d(self):
@@ -53,8 +56,9 @@ class MatrixBuilderTestCase(BW2DataTest):
         row_dict = {1: 0, 2: 1}
         with open(database.filepath_processed(), "wb") as f:
             pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
-        matrix = MatrixBuilder.build(config.dir, ["sour"], "values", "a",
-            "row", row_dict=row_dict, one_d=True)[3]
+        matrix = MatrixBuilder.build(config.dir, [database.filepath_processed()],
+            "values", "a", "row",
+            row_dict=row_dict, one_d=True)[3]
         self.assertTrue(np.allclose(
             matrix.todense(),
             np.array(((99, 0), (0, 100)))
@@ -77,8 +81,9 @@ class MatrixBuilderTestCase(BW2DataTest):
         row_dict = {1: 0, 2: 1}
         with open(database.filepath_processed(), "wb") as f:
             pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
-        values = MatrixBuilder.build(config.dir, ["ghost"], "values", "a",
-            "row", row_dict=row_dict, one_d=True)[0]
+        values = MatrixBuilder.build(config.dir, [database.filepath_processed()],
+            "values", "a", "row",
+            row_dict=row_dict, one_d=True)[0]
         self.assertEqual(values.shape, (2,))
 
     def test_one_d_missing_in_row_dict_raise_valueerror(self):
@@ -98,8 +103,9 @@ class MatrixBuilderTestCase(BW2DataTest):
         with open(database.filepath_processed(), "wb") as f:
             pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
         with self.assertRaises(ValueError):
-            MatrixBuilder.build(config.dir, ["ghost"], "values", "a",
-                "row", row_dict=row_dict, one_d=True, drop_missing=False)
+            MatrixBuilder.build(config.dir, [database.filepath_processed()],
+                "values", "a", "row",
+                row_dict=row_dict, one_d=True, drop_missing=False)
 
     def test_build_drop_missing(self):
         database = Database("boo")
@@ -122,8 +128,8 @@ class MatrixBuilderTestCase(BW2DataTest):
         col_dict = {2: 0, 6: 1}
         with open(database.filepath_processed(), "wb") as f:
             pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
-        values = MatrixBuilder.build(config.dir, ["boo"], "values", "a", "row",
-            "b", "col", row_dict, col_dict)[0]
+        values = MatrixBuilder.build(config.dir, [database.filepath_processed()],
+            "values", "a", "row", "b", "col", row_dict, col_dict)[0]
         self.assertEqual(values.shape, (2,))
 
     def test_missing_in_row_dict_raise_valueerror(self):
@@ -146,8 +152,9 @@ class MatrixBuilderTestCase(BW2DataTest):
         with open(database.filepath_processed(), "wb") as f:
             pickle.dump(array, f, protocol=pickle.HIGHEST_PROTOCOL)
         with self.assertRaises(ValueError):
-            MatrixBuilder.build(config.dir, ["whoah"], "values", "a",
-                "row", "b", "col", row_dict, col_dict, drop_missing=False)
+            MatrixBuilder.build(config.dir, [database.filepath_processed()],
+                "values", "a", "row", "b", "col", row_dict, col_dict,
+                drop_missing=False)
 
     def test_add_matrix_indices(self):
         a = np.arange(10)
