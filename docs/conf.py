64b3b35bcb18e24e7a5b86bd053f7cff7d380741
Chris Mutel <cmutel@gmail.com>
2014-06-11 11:29:58 +0200

    0.15

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@
 # built documents.
 #
 # The short X.Y version.
-version = '0.14'
+version = '0.15'
 # The full version, including alpha/beta/rc tags.
-release = '0.14.1'
+release = '0.15'
 
 import sys
 from os.path import abspath, dirname
