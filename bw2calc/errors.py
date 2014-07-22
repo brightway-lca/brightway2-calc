32d0a29fe78b73ba9d5b68c6f328dca493d6a739
Chris Mutel <cmutel@gmail.com>
2014-07-22 12:24:42 +0200

    Add errors for least squares LCA

--- bw2calc/errors.py
+++ bw2calc/errors.py
@@ -1,3 +1,13 @@
 class OutsideTechnosphere(StandardError):
     """LCA for the given activity, as it is not in the technosphere matrix"""
     pass
+
+
+class EfficiencyWarning(RuntimeWarning):
+    """Least squares is much less efficient than direct computation for square, full-rank matrices"""
+    pass
+
+
+class NoSolutionFound(UserWarning):
+    """No solution to set of linear equations found within given constraints"""
+    pass
