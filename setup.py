002920aa82f1211897d113588fae3a8fa211252a
Chris Mutel <cmutel@gmail.com>
2014-08-03 09:34:34 +0200

    0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bw2calc',
-    version="0.15.1",
+    version="0.16",
     packages=["bw2calc", "bw2calc.tests"],
     author="Chris Mutel",
     author_email="cmutel@gmail.com",
