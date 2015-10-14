b32b85fb5a92458ced2ee724b664255f1b0bc90c
Chris Mutel <cmutel@gmail.com>
2015-10-14 20:15:43 +0200

    Add __all__ to bw2calc

--- bw2calc/__init__.py
+++ bw2calc/__init__.py
@@ -2,11 +2,23 @@
 from __future__ import print_function, unicode_literals
 from eight import *
 
+__all__ = [
+    'GraphTraversal',
+    'LCA',
+    'LeastSquaresLCA',
+    'MatrixBuilder',
+    'MonteCarloLCA',
+    'MultiMonteCarlo',
+    'ParallelMonteCarlo',
+    'ParameterVectorLCA',
+    'TechnosphereBiosphereMatrixBuilder',
+]
+
+__version__ = (1, 1, "dev1")
+
 from .lca import LCA
 from .least_squares import LeastSquaresLCA
 from .monte_carlo import MonteCarloLCA, ParallelMonteCarlo, MultiMonteCarlo
 from .mc_vector import ParameterVectorLCA
 from .graph_traversal import GraphTraversal
 from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
-
-__version__ = (1, 1, "dev1")
