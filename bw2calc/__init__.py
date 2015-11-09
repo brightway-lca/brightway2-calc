3da46bc3f4f9f345d5e6ab8a7e3053e16005c8d2
Chris Mutel <cmutel@gmail.com>
2015-11-09 10:38:44 +0100

    1.1.dev2

--- bw2calc/__init__.py
+++ bw2calc/__init__.py
@@ -12,7 +12,7 @@ __all__ = [
     'TechnosphereBiosphereMatrixBuilder',
 ]
 
-__version__ = (1, 1, "dev1")
+__version__ = (1, 1, "dev2")
 
 from .lca import LCA
 from .least_squares import LeastSquaresLCA
