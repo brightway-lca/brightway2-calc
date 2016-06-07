dfa33aeef4f90b1d467fc8929c5034aff9ce251c
Chris Mutel <cmutel@gmail.com>
2016-06-07 11:24:13 +0200

    Add MC tests

--- tests/monte_carlo.py
+++ tests/monte_carlo.py
@@ -3,12 +3,13 @@ from __future__ import print_function, unicode_literals
 from eight import *
 
 from bw2calc import *
-from bw2data import config, Database, Method
+from bw2data import config, Database, Method, projects, databases
 from bw2data.tests import bw2test
 import pytest
 
 
 @pytest.fixture
+@bw2test
 def background():
     Database("biosphere").write({
         ("biosphere", "1"): {'type': 'emission'},
@@ -55,13 +56,40 @@ def background():
 
 
 def get_args():
-    return {Database("test").random(): 1}, ("a", "method")
+    return {("test", "1"): 1}, ("a", "method")
 
 
-@bw2test
 def test_plain_monte_carlo(background):
-    print(projects.current)
-    print(projects.dir)
-    print(str(databases))
     mc = MonteCarloLCA(*get_args())
+    assert mc.__next__() > 0
+
+def test_monte_carlo_next(background):
+    mc = MonteCarloLCA(*get_args())
+    assert next(mc) > 0
+
+def test_monte_carlo_as_iterator(background):
+    mc = MonteCarloLCA(*get_args())
+    for x in mc:
+        assert x > 0
+        break
+
+def test_direct_solving(background):
+    mc = DirectSolvingMonteCarloLCA(*get_args())
     assert next(mc)
+
+def test_multi_mc(background):
+    mc = MultiMonteCarlo(
+        [
+            {("test", "1"): 1},
+            {("test", "2"): 1},
+            {("test", "1"): 1, ("test", "2"): 1}
+        ],
+        ("a", "method"),
+        iterations=10
+    )
+    assert mc.calculate()
+
+def test_parallel_monte_carl(background):
+    fu, method = get_args()
+    mc = ParallelMonteCarlo(fu, method, iterations=200)
+    assert mc.calculate()
