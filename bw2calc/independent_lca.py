945bccbfdb250157702b394c07f3f0861aef0820
Jan <jmachacek@eaternity.ch>
2019-09-12 16:45:23 +0200

    adjusted IndependentLCAMixin to allow numpy array passthrough

--- bw2calc/independent_lca.py
+++ bw2calc/independent_lca.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, division
+
+import numpy as np
 from eight import *
 import os
 
 
 class IndependentLCAMixin(object):
-    """Mixin that allows `method`, etc. to be filepaths instead of DataStore object names.
+    """Mixin that allows `method`, etc. to be filepaths or ``np.ndarray`` instead of DataStore object names.
 
     Removes dependency on `bw2data`."""
     def get_array_filepaths(self):
@@ -13,8 +15,9 @@ class IndependentLCAMixin(object):
         assert self.database_filepath, "Must specify `database_filepath` in independent LCA"
         for collection in (self.database_filepath, self.method, self.weighting, self.normalization):
             if collection is not None:
-                for fp in collection:
-                    assert os.path.exists(fp), "Can't find file {}".format(fp)
+                for obj in collection:
+                    if not isinstance(obj, np.ndarray):
+                        assert os.path.exists(obj), "Can't find file {}".format(obj)
         return (
             self.database_filepath,
             self.method,
