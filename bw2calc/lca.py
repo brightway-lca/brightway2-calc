19251e48f7fcb21118379a41b1caff5ccac53eb4
Pascal Lesage <pascal.lesage@polymtl.ca>
2020-02-26 12:40:15 -0500

    Fix lci_redo and lcia_redo for case with no new demand

--- bw2calc/lca.py
+++ bw2calc/lca.py
@@ -511,8 +511,8 @@ Note that this is a `property <http://docs.python.org/2/library/functions.html#p
         assert hasattr(self, "inventory"), "Must do lci first"
         if demand is not None:
             self.build_demand_array(demand)
+            self.demand = demand
         self.lci_calculation()
-        self.demand = demand
         self.logger.info("Redoing LCI", extra={'demand': wrap_functional_unit(demand or self.demand)})
 
     def redo_lcia(self, demand=None):
@@ -527,8 +527,8 @@ Note that this is a `property <http://docs.python.org/2/library/functions.html#p
         assert hasattr(self, "characterized_inventory"), "Must do LCIA first"
         if demand is not None:
             self.redo_lci(demand)
+            self.demand = demand
         self.lcia_calculation()
-        self.demand = demand
         self.logger.info("Redoing LCIA", extra={'demand': wrap_functional_unit(demand or self.demand)})
 
     def to_dataframe(self, cutoff=200):
