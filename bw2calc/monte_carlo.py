f0390dd194979d7157ea20b9ef208ca236a4e0fb
Chris Mutel <cmutel@gmail.com>
2023-02-24 22:15:27 +0100

    Improve deprecation warning message

--- bw2calc/monte_carlo.py
+++ bw2calc/monte_carlo.py
@@ -10,6 +10,7 @@ from contextlib import contextmanager
 from scipy.sparse.linalg import cgs
 from stats_arrays.random import MCRandomNumberGenerator
 import multiprocessing
+import platform
 import sys
 import warnings
 
@@ -183,12 +184,13 @@ class ParallelMonteCarlo(object):
     """Split a Monte Carlo calculation into parallel jobs"""
     def __init__(self, demand, method, iterations=1000, chunk_size=None,
                  cpus=None, log_config=None):
-        MSG = (
-            "The ParallelMonteCarlo class is deprecated; as of Python 3.8 it doesn't "
-            "work reliably on Windows or MacOS, and doesn't offer speed advantages "
-            "when using the Pardiso solver"
-        )
-        warnings.warn(MSG, DeprecationWarning)
+        if platform.system() in ("Windows", "Darwin"):
+            MSG = (
+                "The ParallelMonteCarlo class is deprecated; as of Python 3.8 it doesn't "
+                "work reliably on Windows or MacOS, and doesn't offer speed advantages "
+                "when using the Pardiso solver"
+            )
+            warnings.warn(MSG, DeprecationWarning)
 
         clean_databases()
         self.demand = demand
