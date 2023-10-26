88ca53b3f83bfe609a47c43d5dd69d4df8013d30
Chris Mutel <cmutel@gmail.com>
2023-10-26 11:58:07 +0200

    Update single_value_diagonal_matrix.py

--- bw2calc/single_value_diagonal_matrix.py
+++ bw2calc/single_value_diagonal_matrix.py
@@ -9,11 +9,18 @@ from .errors import MultipleValues
 
 
 class SingleValueDiagonalMatrix(MappedMatrix):
-    """A scipy sparse matrix handler which takes in ``bw_processing`` data packages. Row and column ids are mapped to matrix indices, and a matrix is constructed.
+    """A scipy sparse matrix handler which takes in ``bw_processing`` data packages. Row and column
+    ids are mapped to matrix indices, and a matrix is constructed.
 
-    `indexer_override` allows for custom indexer behaviour. Indexers should follow a simple API: they must support `.__next__()`, and have the attribute `.index`, which returns an integer.
+    Use primarily in the weighting step of life cycle impact assessment.
 
-    `custom_filter` allows you to remove some data based on their indices. It is applied to all resource groups. If you need more fine-grained control, process the matrix after construction/iteration. `custom_filter` should take the indices array as an input, and return a Numpy boolean array with the same length as the indices array.
+    `indexer_override` allows for custom indexer behaviour. Indexers should follow a simple API:
+    they must support `.__next__()`, and have the attribute `.index`, which returns an integer.
+
+    `custom_filter` allows you to remove some data based on their indices. It is applied to all
+    resource groups. If you need more fine-grained control, process the matrix after
+    construction/iteration. `custom_filter` should take the indices array as an input, and return a
+    Numpy boolean array with the same length as the indices array.
 
     Args:
 
@@ -61,9 +68,10 @@ class SingleValueDiagonalMatrix(MappedMatrix):
 
         if self.raw_data.shape != (1,):
             raise MultipleValues(
-                "Multiple ({}) numerical values found, but only one single numerical value is allowed. Data packages:\n\t{}".format(
-                    len(self.raw_data), "\n\t".join([str(x) for x in self.packages])
-                )
+                (
+                    "Multiple ({}) numerical values found, but only one single numerical value is "
+                    + "allowed. Data packages:\n\t{}"
+                ).format(len(self.raw_data), "\n\t".join([str(x) for x in self.packages]))
             )
 
     def rebuild_matrix(self):
