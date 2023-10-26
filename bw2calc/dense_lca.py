1f45c0ee743c729c4d3a51e3646fcfa02d919e2b
Chris Mutel <cmutel@gmail.com>
2023-10-26 11:52:20 +0200

    Update docstring for dense LCA

--- bw2calc/dense_lca.py
+++ bw2calc/dense_lca.py
@@ -4,17 +4,7 @@ from .lca import LCA
 
 
 class DenseLCA(LCA):
+    """Convert the `technosphere_matrix` to a numpy array and solve with `numpy.linalg`."""
+
     def solve_linear_system(self):
-        """
-        Master solution function for linear system :math:`Ax=B`.
-
-            To most numerical analysts, matrix inversion is a sin.
-
-            -- Nicolas Higham, Accuracy and Stability of Numerical Algorithms, Society for Industrial and Applied Mathematics, Philadelphia, PA, USA, 2002, p. 260.
-
-        We use `UMFpack <http://www.cise.ufl.edu/research/sparse/umfpack/>`_, which is a very fast solver for sparse matrices.
-
-        If the technosphere matrix has already been factorized, then the decomposed technosphere (``self.solver``) is reused. Otherwise the calculation is redone completely.
-
-        """
         return solve(self.technosphere_matrix.toarray(), self.demand_array)
